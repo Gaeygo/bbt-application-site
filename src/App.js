99757aeaf0d1e08a848408eee2708fe970c35127
Great <Gaeygo@gmail.com>
2022-04-28 13:29:21 +0100

    Import Footer component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import {motion} from "framer-motion"
 import './App.css';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
        <Navbar/>
+       <Footer/>
     </div>
   );
 }
