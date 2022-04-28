7cb4c2548b97e40219cc56fc2f03ca8994011ff2
Great <Gaeygo@gmail.com>
2022-04-28 13:28:59 +0100

    Footer component created

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
-import {} from "@chakra-ui/react"
+import React from "react";
+import { Flex, Text, Image, Stack, Box } from "@chakra-ui/react";
+import Logo from "../images/esa.png";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <Flex mx="auto" mt="158px" mb="133px" w="80%" direction="column">
+      <Flex fontSize="18px" fontWeight={400}  justify="space-between">
+        <Box>
+          <Image mb="16px" h="33px" w="94px" src={Logo} />
+          <Text>Helping business break threshold</Text>
+        </Box>
+        <Flex gap="70px" textAlign="left">
+          <Stack>
+            <Text fontSize="24px" fontWeight={600}>
+              Contact
+            </Text>
+            <Text>hello@esa.com</Text>
+            <Text>+234 800 1234 567</Text>
+          </Stack>
+          <Stack>
+            <Text fontSize="24px" fontWeight={600}>
+              Contact
+            </Text>
+            <Text>hello@esa.com</Text>
+            <Text>+234 800 1234 567</Text>
+          </Stack>{" "}
+          <Stack>
+            <Text fontSize="24px" fontWeight={600}>
+              Contact
+            </Text>
+            <Text>hello@esa.com</Text>
+            <Text>+234 800 1234 567</Text>
+          </Stack>
+        </Flex>
+      </Flex>
+      <Flex justify="space-between" mt="260px">
+        <Text>ESA 2022. All rights reserved</Text>
+        <Stack direction="row">
+          <Text>Twitter</Text>
+          <Text>Facebook</Text>
+          <Text>Instagram</Text>
+        </Stack>
+      </Flex>
+    </Flex>
+  );
+};
 
-export default Footer
+export default Footer;
