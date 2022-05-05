a5755ef69a8c40c873bed851d45e7b22e5d5e7aa
Great <Gaeygo@gmail.com>
2022-05-05 12:03:24 +0100

    responsive

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -7,13 +7,25 @@ import cloud from "../images/cloud.svg";
 const Homepage = () => {
   return (
     <>
-      <Flex direction={{base: "column", md: "row"}} justify="space-between" w="85%" mx="auto" mt="31px">
-        <Box border="2px solid #04538B" borderRadius="5px">
+      <Flex
+        direction={{ base: "column", md: "row" }}
+        justify="space-between"
+        w="85%"
+        mx="auto"
+        mt="31px"
+      >
+        <Box
+          w={{ md: "400px" }}
+          maxW={{ base: "350px", md: "400px" }}
+          h={{ base: "350px", md: "378px" }}
+          border="2px solid #04538B"
+          borderRadius="5px"
+        >
           <Box
             borderRadius="5px"
-            w={{md: "400px"}}
-            maxW={{base:"350px",md:"400px"}}
-            h={{base: "350px",md:"378px"}}
+            w={{ md: "400px" }}
+            maxW={{ base: "350px", md: "400px" }}
+            h={{ base: "350px", md: "378px" }}
             backgroundColor="#04538B"
             position="relative"
             left="20px"
@@ -25,18 +37,32 @@ const Homepage = () => {
           textAlign="left"
           my="auto"
           direction="column"
-          w={{base:"100%",md:"51%"}}
+          w={{ base: "100%", md: "51%" }}
         >
-          <Text mt={{base: "32px", md:"0"}} fontSize={{base: "32px",md:"48px"}} fontWeight={500}>
+          <Text
+            mt={{ base: "32px", md: "0" }}
+            fontSize={{ base: "32px", md: "48px" }}
+            fontWeight={500}
+          >
             Start using the cloud for your business today
           </Text>
-          <Text mt="10px" mb={{base: "50px",md:"22px"}} fontSize={{base: "14px",md:"20px" }}fontWeight={400}>
+          <Text
+            mt="10px"
+            mb={{ base: "50px", md: "22px" }}
+            fontSize={{ base: "14px", md: "20px" }}
+            fontWeight={400}
+          >
             ESA is a department under the BBT Enterprise a company that is blah
             blah blah{" "}
           </Text>
-          <Flex gap={{base: "20px", md: "0"}} w="70%" direction="row" mb={{base: "120px", md: "0"}} >
+          <Flex
+            gap={{ base: "20px", md: "0" }}
+            w="70%"
+            direction="row"
+            mb={{ base: "120px", md: "0" }}
+          >
             <Button
-              w={{base: "170px",md:"400px"}}
+              w={{ base: "170px", md: "400px" }}
               h="45px"
               borderRadius="4px"
               backgroundColor="#04538B"
@@ -52,8 +78,7 @@ const Homepage = () => {
               Learn More
             </Button>
             <Button
-              w={{base: "170px",md:"400px"}}
-
+              w={{ base: "170px", md: "400px" }}
               h="45px"
               borderRadius="4px"
               backgroundColor="white"
@@ -67,10 +92,18 @@ const Homepage = () => {
       </Flex>
       <Sponsors />
       <Stack mt="200px" w="85%" mx="auto">
-        <Text fontSize={{base: "22px",md:"28px"}} fontWeight={600} textAlign="center">
+        <Text
+          fontSize={{ base: "22px", md: "28px" }}
+          fontWeight={600}
+          textAlign="center"
+        >
           Providing essential tools & sevices for your business{" "}
         </Text>
-        <Text fontSize={{base: "16px",md:"18px"}} fontWeight={500} textAlign="center">
+        <Text
+          fontSize={{ base: "16px", md: "18px" }}
+          fontWeight={500}
+          textAlign="center"
+        >
           We Provide a wide array of services targetted at business owners to
           efficiently carry out their day to day activities without stress and
           wahala .
@@ -79,7 +112,7 @@ const Homepage = () => {
       <Flex
         mt="120px"
         w="85%"
-        gap={{base: "120px",md:"60px"}}
+        gap={{ base: "120px", md: "60px" }}
         mx="auto"
         flexWrap="wrap"
         alignItems="center"
