167f6e61e82066b4f08d64f0270a1edcdb655d65
Great <Gaeygo@gmail.com>
2022-05-05 11:58:29 +0100

    deletions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import logo from "./logo.svg";
-import { motion } from "framer-motion";
+
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Sponsors from "./components/Sponsors";
 import Homepage from "./components/Homepage";
 
 function App() {
