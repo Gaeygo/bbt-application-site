e97b00c9fd34400c33b203382ef4f42abff4025c
Great <Gaeygo@gmail.com>
2022-05-10 13:42:41 +0100

    dead link  fix

--- src/components/_404.jsx
+++ src/components/_404.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Flex, Text, chakra,Link } from "@chakra-ui/react";
+import {useNavigate} from "react-router-dom"
 
 const _404 = () => {
+    const navigate = useNavigate()
   return (
     <Flex mt="225px" direction="column">
       <Text fontSize="128px" fontWeight={600} textColor="#141313" letterSpacing={-12.5} >404</Text>
@@ -9,7 +11,7 @@ const _404 = () => {
       <Text mx="auto" w="65%" fontSize="24px" fontWeight={500}>
         Oops the page you are looking for doesn’t exist. Find your way to the
         
-        <chakra.span textColor="#04538B" ><Link> homepage</Link></chakra.span>
+        <chakra.span textColor="#04538B" ><Link textDecoration="none" _hover={{textDecoration: "none"}} onClick={() => navigate("/")}> homepage</Link></chakra.span>
       </Text>
     </Flex>
   );
